a1071ac3e1daf8a91285301b56ee9c4cb00fe1bc
KauanAfonso <kauanafon3@gmail.com>
2024-12-25 12:48:04 -0300

    encapsulando

--- amigo_secreto.py
+++ amigo_secreto.py
@@ -4,9 +4,9 @@ class Amigo_secreto:
 
     #construtora
     def __init__(self):
-        self.lista_nomes = []
-        self.lista_sorteio = []
-        self.dicionario_de_nomes = {}
+        self.__lista_nomes = []
+        self.__lista_sorteio = []
+        self.__dicionario_de_nomes = {}
         
     #funcão para mensagem no inicio do jogo    
     def mensagem_inicial(self):
@@ -28,28 +28,28 @@ class Amigo_secreto:
             if nomes == "0":
                 print("Nomes enviados...")
                 break
-            self.lista_nomes.append(nomes)
-            self.lista_sorteio.append(nomes)
+            self.__lista_nomes.append(nomes)
+            self.__lista_sorteio.append(nomes)
             i+=1
-        return self.lista_nomes, self.lista_sorteio
+        return self.__lista_nomes, self.__lista_sorteio
     
     #funcao de sorteio
     def sortear(self):
         while True:                                  # Loop até um sorteio válido acontecer
-            lista_sorteio = self.lista_nomes.copy()  # Faz uma cópia da lista de nomes
+            lista_sorteio = self.__lista_nomes.copy()  # Faz uma cópia da lista de nomes
             random.shuffle(lista_sorteio)            # Embaralha a cópia
 
-            self.dicionario_de_nomes.clear()         # Limpa o sorteio anterior (se existir)
-            for i in self.lista_nomes:               # Para cada participante:
+            self.__dicionario_de_nomes.clear()         # Limpa o sorteio anterior (se existir)
+            for i in self.__lista_nomes:               # Para cada participante:
                 if lista_sorteio[-1] == i:           # Evita que alguém tire a si mesmo
                     random.shuffle(lista_sorteio)    # Se for o caso, embaralha de novo
-                self.dicionario_de_nomes[i] = lista_sorteio.pop()  # Atribui o sorteio
+                self.__dicionario_de_nomes[i] = lista_sorteio.pop()  # Atribui o sorteio
             
             # Verifica se o sorteio foi válido (ninguém tirou a si mesmo)
-            if all(k != v for k, v in self.dicionario_de_nomes.items()):
+            if all(k != v for k, v in self.__dicionario_de_nomes.items()):
                 break                                # Se o sorteio for válido, sai do loop
         
-        return self.dicionario_de_nomes
+        return self.__dicionario_de_nomes
 
     #funcão que irá mostrar quem o jogador tirou no sorteio
     def mostrar_sorteio(self):
@@ -58,7 +58,7 @@ class Amigo_secreto:
             if resposta == "0":
                 print("JOGO FINALIZADO !!!!")
                 break
-            print(f" {resposta} você tirou o(a): {self.dicionario_de_nomes[resposta]}")
+            print(f" {resposta} você tirou o(a): {self.__dicionario_de_nomes[resposta]}")
             input("---------Pressione ENTER--------------")
             subprocess.run('cls', shell=True)
     
